c7b58190277ba93f588dfa73248563b837f98519
li-shuai <506507881@qq.com>
2025-06-25 00:27:13 +0800

    feat:java基础语法&&java注释

---
+++ src/cn/happy/basic_syntax/T1.java
@@ -0,0 +1,7 @@
+package cn.happy.basic_syntax;
+
+public class T1 {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
---
+++ src/cn/happy/basic_syntax/T2.java
@@ -0,0 +1,22 @@
+package cn.happy.basic_syntax;
+
+public class T2 {
+    public static void main(String[] args) {
+        // 这是一个单行注释
+        int x = 10;
+        System.out.println("//打印一个单行注释");
+
+        /*
+        这是一个多行注释
+        可以用来注释多行代码
+         */
+    }
+
+    /**
+     * 这是一个文档注释示例
+     * 它通常包含有关类、方法或字段的详细信息
+     */
+    public class MyClass {
+        // 类的成员和方法
+    }
+}
