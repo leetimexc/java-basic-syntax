00bb14eb348870ea542ec3959d7044f676c3f5f9
li-shuai <506507881@qq.com>
2025-06-27 02:38:33 +0800

    基础类型：datatypes

---
+++ src/cn/happy/basic_datatypes/T1.java
@@ -0,0 +1,26 @@
+package cn.happy.basic_datatypes;
+
+public class T1 {
+    public static void main(String[] args) {
+        // byte
+        System.out.println("基本类型：byte 二进制位数"+Byte.SIZE);
+        System.out.println("包装类：java.lang.Byte");
+        System.out.println("最小值：Byte.MIN_VALUE="+Byte.MIN_VALUE);
+        System.out.println("最大值：Byte.MAX_VALUE="+Byte.MAX_VALUE);
+        System.out.println();
+
+        // short
+        System.out.println("基本类型： short 二进制位数"+Short.SIZE);
+        System.out.println("包装类 java.lang.Short");
+        System.out.println("最小值Short.MIN_VALUE="+Short.MIN_VALUE);
+        System.out.println("最大值 Short.MAX_VALUE="+Short.MAX_VALUE);
+        System.out.println();
+
+        // int
+        System.out.println("基本类型：int 二进制位数"+Integer.SIZE);
+        System.out.println("包装类 java.lang.Integer");
+        System.out.println("最小值Integer.MIN_VALUE="+Integer.MIN_VALUE);
+        System.out.println("最大值 Integer.MAX_VALUE="+Integer.MAX_VALUE);
+        System.out.println();
+    }
+}
