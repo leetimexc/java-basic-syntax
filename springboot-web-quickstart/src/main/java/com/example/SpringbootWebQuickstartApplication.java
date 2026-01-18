8e8781ed289611c6be8dd0e2ff4d03815483cd47
li-shuai <506507881@qq.com>
2026-01-19 01:18:17 +0800

    feat: 提交springboot创建项目

--- springboot-web-quickstart/src/main/java/com/example/SpringbootWebQuickstartApplication.java
+++ springboot-web-quickstart/src/main/java/com/example/SpringbootWebQuickstartApplication.java
@@ -3,6 +3,9 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类/引导类
+ */
 @SpringBootApplication
 public class SpringbootWebQuickstartApplication {
 
