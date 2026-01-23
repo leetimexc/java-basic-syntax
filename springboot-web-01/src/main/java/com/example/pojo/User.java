e38940775bb89a2a5700d6e33bc3e1820542f714
li-shuai <506507881@qq.com>
2026-01-23 14:44:35 +0800

    feat: 处理list接口

--- springboot-web-01/src/main/java/com/example/pojo/User.java
+++ springboot-web-01/src/main/java/com/example/pojo/User.java
@@ -1,10 +1,8 @@
 package com.example.pojo;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
 /*
@@ -15,10 +13,20 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class User {
 
-    private Integer id;
-    private String username;
-    private String password;
-    private String name;
-    private Integer age;
-    private LocalDateTime updateTime;
+  private Integer id;
+  private String username;
+  private String password;
+  private String name;
+  private Integer age;
+  private LocalDateTime updateTime;
+
+  // public User(Integer id, String username, String password, String name,
+  // Integer age, LocalDateTime updateTime) {
+  // this.id = id;
+  // this.username = username;
+  // this.password = password;
+  // this.name = name;
+  // this.age = age;
+  // this.updateTime = updateTime;
+  // }
 }
