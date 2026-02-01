198a309779ed7f23bb2ad4d93e87426f465f82dd
li-shuai <506507881@qq.com>
2026-02-02 00:50:20 +0800

    feat: sql语句

--- jdbc-demo/src/test/java/JDBCTest.java
+++ jdbc-demo/src/test/java/JDBCTest.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
 public class JDBCTest {
+
+    /**
+     * 编写JDBC程序, 查询数据
+     */
+    @Test
+    public void testJdbc() throws Exception {
+        // 获取连接
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/web", "root", "1234");
+        // 创建预编译的PreparedStatement对象
+        PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM user WHERE username = ? AND password = ?");
+        // 设置参数
+        pstmt.setString(1, "daqiao"); // 第一个问号对应的参数
+        pstmt.setString(2, "123456"); // 第二个问号对应的参数
+        // 执行查询
+        ResultSet rs = pstmt.executeQuery();
+        // 处理结果集
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String uName = rs.getString("username");
+            String pwd = rs.getString("password");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+
+            System.out.println("ID: " + id + ", Username: " + uName + ", Password: " + pwd + ", Name: " + name + ", Age: " + age);
+        }
+        // 关闭资源
+        rs.close();
+        pstmt.close();
+        conn.close();
+    }
+
 }
---
+++ mysql/README.md
@@ -0,0 +1,42 @@
+### 连接本地数据库
+`mysql -uroot -p****`
+
+### 连接远程数据库
+`mysql -h192.168.1.1 -P8080 -uroot -p****`
+
+### 创建表
+```
+    create table tableUser(
+        id int comment 'ID,唯一标识',
+        username varchar(50) comment '用户名',
+        name varchar(10) comment '姓名',
+        age int comment '年龄',
+        gender char(1) comment '性别'
+    ) comment '用户信息表';
+```
+
+当前可以创建多个id相同的数据，所以需要约束
+
+### 约束
+| 约束     | 描述               | 关键字      |
+|----------|--------------------|-------------|
+| 非空约束 | 限制该字段值不能为null | not null    |
+| 唯一约束 | 保证字段的所有数据都是唯一、不重复的    | unique      |
+| 主键约束 | 主键是一行数据的唯一标识，要求非空且唯一 | primary key |
+| 默认约束 | 保存数据时，如果未指定该字段值，则采用默认值    | default     |
+| 外键约束 | 让两张表的数据建立连接，保证数据的一致性和完整性 | foreign key |
+
+### 带要约束的表创建
+```
+create table tb_user (
+    id int primary key comment 'ID,唯一标识', 
+    username varchar(20) not null unique comment '用户名',
+    name varchar(10) not null comment '姓名',
+    age int comment '年龄',
+    gender char(1) default '男' comment '性别'
+) comment '用户表';
+```
+当前ID相对麻烦，需要每次添加的时候手动维护。
+
+### 自增
+`auto_increment`
